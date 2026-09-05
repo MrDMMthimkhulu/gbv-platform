a973a2ae4aa5292802f83e6c74832869155c3a5e
MrDMMthimkhulu <denmmthimkhulu@gmail.com>
2026-09-05 17:30:38 +0200

    Expand locales in next-i18next configuration

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zu'],
+    locales: ['en', 'af', 'nr', 'xh', 'zu', 'nso', 'st', 'tn', 'ss', 've', 'ts'],
   },
   localePath: path.resolve('./public/locales'),
 };
