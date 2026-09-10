c62ee24daf7806c9eccc522a21e4c879b76a2786
MrDMMthimkhulu <denmmthimkhulu@gmail.com>
2026-09-10 04:56:28 +0200

    Update index.js

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -161,18 +161,7 @@ export default function LearnPage() {
       </Head>
 
       <div
-        style={
-          isGirl
-            ? {
-                '--rose': '#f97316',
-                '--rose-deep': '#c2410c',
-                '--blush': '#ffedd5',
-                '--teal': '#eab308',
-                '--teal-light': '#fef9c3',
-                '--warm': '#fed7aa',
-                '--ink': '#7c2d12',
-              }
-            : undefined
+      <div>
         }
       >
 
